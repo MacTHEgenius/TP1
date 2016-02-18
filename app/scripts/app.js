b0123bc01af98df7a8c08ecc23833f2098f2eabd
Benoît Lévesque <benoitlevesque@csfnet-016-242.cegep-ste-foy.qc.ca>
2016-02-18 17:56:39 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tp1App
+ * @description
+ * # tp1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tp1App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
