41ad600dfb2e0072642b9a795e45ed99712a73f4
XavierCL <xavierx-men@hotmail.com>
2016-02-23 13:50:07 -0500

    Validate Inscription commit

---
+++ test/spec/controllers/Inscription.js
@@ -0,0 +1,24 @@
+'use strict';
+
+describe('Controller: inscriptionController', function () {
+
+  // load the controller's module
+  beforeEach(module('tp1App'));
+
+  var inscriptionController,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    inscriptionController = $controller('inscriptionController', {
+      $scope: scope
+      // place here mocked dependencies
+    });
+  }));
+
+  it('should initialize default show values', function () {
+    expect(scope.showForm).toBe(true);
+    expect(scope.showSubmitError).toBe(false);
+  });
+});
