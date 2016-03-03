1137f8fcc780fdea869a9db08c8b727f1dd0bf78
XavierCL <xavierx-men@hotmail.com>
2016-03-03 17:16:55 -0500

    add directive

--- client/app/inscription/inscription.controller.js
+++ client/app/inscription/inscription.controller.js
@@ -11,7 +11,7 @@ var myApp=angular.module('webApp');
 myApp.controller('inscriptionController', function ($scope) {
 	$scope.showForm=true;
 	$scope.isValid=function(){
-    	return $scope.inscriptionForm.$valid && $scope.passwordM==$scope.confirmPasswordM;
+    	return $scope.inscriptionForm.$valid;// && $scope.passwordM==$scope.confirmPasswordM;
 	};
 	$scope.submit=function(){
 		if(!$scope.isValid())
@@ -25,3 +25,21 @@ myApp.controller('inscriptionController', function ($scope) {
 		else {$scope.showForm=false;}
 	};
   });
+myApp.directive("confirmPassword", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=confirmPassword"
+        },
+        link: function(scope, element, attributes, ngModel) {
+             
+            ngModel.$validators.confirmPassword = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+ 
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+});
