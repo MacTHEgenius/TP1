9d13b9b4b6c259ce80b21bcf8732b61ec7d6e058
XavierCL <xavierx-men@hotmail.com>
2016-03-29 14:04:12 -0400

    initiate files

---
+++ client/app/favorites/favorites.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('webApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/favorites', {
+        templateUrl: 'app/favorites/favorites.html',
+        controller: 'favoritesController'
+      });
+  });
